1a6e5d5d2cf04327d1fba92c9a5ae1ef5c5a8311
Samuel Ocran <sam.ocran@wakanow.local>
2019-02-05 17:06:02 +0100

    Project setup - for creating UI

---
+++ UI/gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  browserSync = require("browser-sync").create(),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import"),
+  mixins = require("postcss-mixins");
+
+gulp.task("styles", function() {
+  return gulp
+    .src("./assets/pcss/main.css")
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on("error", function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit("end");
+    })
+    .pipe(gulp.dest("./temp/styles"));
+});
+
+gulp.task("watch", function() {
+  browserSync.init({
+    //notify: false,
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  watch("./index.html", function() {
+    browserSync.reload();
+  });
+
+  watch("./assets/pcss/**/*.css", function() {
+    gulp.start("cssInject");
+  });
+
+  watch("./assets/pcss/**/*.pcss", function() {
+    gulp.start("cssInject");
+  });
+});
+
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./temp/styles/main.css").pipe(browserSync.stream());
+});
