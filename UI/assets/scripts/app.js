a0de05dcbd71edcb371538e25c4012cdacabad20
Samuel Ocran <sam.ocran@wakanow.local>
2019-02-07 07:44:31 +0100

    Upgrade to gulp4: gulpfile refactored from g3 to g4

---
+++ UI/assets/scripts/app.js
@@ -0,0 +1,15 @@
+import Person from './modules/Person';
+// const Person = require('./modules/Person');
+// "webpack-cli": "^3.2.3"
+// const name = "Ocran";
+
+// alert(name);
+
+
+
+const guy = new Person("Di", "blue");
+
+
+guy.greet();
+
+
--- UI/gulpfile.js
+++ UI/gulpfile.js
@@ -6,9 +6,17 @@ const gulp = require("gulp"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
   cssImport = require("postcss-import"),
-  mixins = require("postcss-mixins");
+  mixins = require("postcss-mixins"),
+  webpack = require("webpack");
 
-gulp.task("styles", function () {
+gulp.task('styles', postcss_to_css);
+
+gulp.task('scripts', bundle_scripts);
+
+gulp.task('watch', live_edit);
+
+//Converts postcss/scss to css
+function postcss_to_css() {
   return gulp
     .src("./assets/styles/main.css")
     .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
@@ -17,9 +25,32 @@ gulp.task("styles", function () {
       this.emit("end");
     })
     .pipe(gulp.dest("./temp/styles"));
-});
+}
 
-gulp.task("watch", function () {
+//Converts & bundles es6 to es5
+function bundle_scripts(done) {
+  webpack(require("./webpack.config.js"), (err, stats) => {
+    if (err)
+      console.log(err.toString());
+
+    console.log(stats.toString());
+  });
+  done();
+};
+
+
+function inject_css() {
+  return gulp.src("./temp/styles/main.css").pipe(browserSync.stream());
+}
+
+function reload_browser(done) {
+  browserSync.reload();
+  done();
+}
+
+
+function live_edit(done) {
+
   browserSync.init({
     //notify: false,
     server: {
@@ -27,19 +58,14 @@ gulp.task("watch", function () {
     }
   });
 
-  watch("./*.html", function () {
-    browserSync.reload();
-  });
+  watch("./*.html", reload_browser);
+  watch("./assets/styles/**/*.css", gulp.series(postcss_to_css, inject_css));
+  watch("./assets/styles/**/*.pcss", gulp.series(postcss_to_css, inject_css));
+  watch("./assets/scripts/**/*.js", gulp.series(bundle_scripts, reload_browser));
 
-  watch("./assets/styles/**/*.css", function () {
-    gulp.start("cssInject");
-  });
+  done();
+};
 
-  watch("./assets/styles/**/*.pcss", function () {
-    gulp.start("cssInject");
-  });
-});
 
-gulp.task("cssInject", ["styles"], function () {
-  return gulp.src("./temp/styles/main.css").pipe(browserSync.stream());
-});
+
+
